5c5e22fe7185ec96e662b987d706d527fe46416c
Sulaimon <maito4me@gmail.com>
2023-08-16 15:25:47 +0100

    controllers moved to the controller file.

--- api.router.js
+++ api.router.js
@@ -9,42 +9,11 @@ router.get('/', controller.GetStudents)
 
 router.post('/', middleware.CheckProgram, controller.CreateStudents)
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id 
-    const students = controller.students
-    const foundStudent = students.find((student)=>{
-        return student.id == parseInt(id)
-    })
-    if(!foundStudent){
-        res.status(404).send(`Item not found`)
-    }
-    res.status(200).json(foundStudent)
-})
+router.get('/:id', controller.getOneStudent)
 
-router.patch("/:id", (req,res)=>{
-    const id = req.params.id
-    const update = req.body
-    const students = controller.students
-    const foundIndex = students.findIndex(student=>student.id == parseInt(id))
-    if(foundIndex== -1){
-        res.end(`student with id ${id} is not found`)
-        return
-    }
-    students[foundIndex] = {...students[foundIndex], ...update}
-    res.status(200).json(students[foundIndex])
-})
+router.patch("/:id", controller.updateStudent)
+    
+router.delete("/:id", controller.deleteStudent )
 
-
-router.delete("/:id", (req,res)=>{
-    const id = req.params.id
-    const students = controller.students
-    const foundIndex = students.findIndex(student=>student.id == parseInt(id))
-    if(foundIndex== -1){
-        res.end(`student with id:${id} is not found`)
-        return
-    }
-    students.splice(foundIndex, 1)
-    res.end(`student with id:${id}, deleted successfully`)
-})
 
 module.exports = router
